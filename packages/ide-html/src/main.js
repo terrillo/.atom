195223f3ddc14c87cc7be7826faa2b737c9ca7f2
Terrillo Walls <terrillo@terrillo.com>
2021-11-12 18:33:26 -0600

    Marketing setup

---
+++ packages/ide-html/src/main.js
@@ -0,0 +1,45 @@
+const path = require('path')
+const { AutoLanguageClient } = require('atom-languageclient')
+const { registerConfigOnChangeHandlers, registerOpenSettingsCommand } = require('./util')
+const { registerHelpCommands } = require('./helpCmd')
+
+class HTMLLanguageClient extends AutoLanguageClient {
+  constructor() {
+    super()
+    registerConfigOnChangeHandlers()
+    registerOpenSettingsCommand()
+    registerHelpCommands()
+  }
+  getGrammarScopes () {
+    const {
+      additionalGrammars,
+      gohtmlSupport,
+      mustacheSupport,
+      phphtmlSupport,
+    } = atom.config.get('ide-html')
+    return ['text.html.basic']
+      .concat(gohtmlSupport ? 'text.html.gohtml' : [])
+      .concat(mustacheSupport ? 'text.html.mustache' : [])
+      .concat(phphtmlSupport ? 'text.html.php' : [])
+      .concat(additionalGrammars || [])
+  }
+  getLanguageName () { return 'HTML' }
+  getServerName () { return 'VSCODE-HTML-LANG-SERVER' }
+  getConnectionType() { return 'stdio' } // ipc, socket, stdio
+
+  startServerProcess () {
+    return super.spawnChildNode([
+      path.resolve(path.join(
+        __dirname,
+        '../node_modules/vscode-html-languageserver-bin/htmlServerMain'
+      )),
+      '--stdio',
+    ]) // --node-ipc, stdio, socket={number}
+  }
+
+  preInitialization (connection) {
+    connection.onCustom('$/partialResult', () => {}) // Suppress partialResult until the language server honours 'streaming' detection
+  }
+}
+
+module.exports = new HTMLLanguageClient()
---
+++ packages/ide-html/src/util.js
@@ -0,0 +1,32 @@
+'use babel'
+
+import packageJSON from '../package.json'
+
+export const registerConfigOnChangeHandlers = () => {
+  atom.config.onDidChange('ide-html.additionalGrammars', () =>
+    promptUserReloadAtom('Reload `ide-html` to apply additional grammars')
+  )
+  atom.config.onDidChange('ide-html.jspSupport', () => promptUserReloadAtom() )
+  atom.config.onDidChange('ide-html.mustacheSupport', () => promptUserReloadAtom() )
+  atom.config.onDidChange('ide-html.phphtmlSupport', () => promptUserReloadAtom())
+}
+
+export const registerOpenSettingsCommand = () => {
+  atom.commands.add('atom-workspace', `${packageJSON.name}:open-settings`, () => {
+    atom.workspace.open(`atom://config/packages/${packageJSON.name}`)
+  })
+}
+
+export const promptUserReloadAtom = (msg = 'Reload `ide-html` to apply changes') => {
+  const buttons = [{
+    text: 'Reload',
+    onDidClick: () => atom.reload(),
+  }]
+  atom.notifications.addInfo(
+    msg,
+    {
+      buttons,
+      dismissable: true,
+    }
+  )
+}
